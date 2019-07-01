c444a14a28c000e631f9874eda58a3c884f3ee7e
Andrew <astencavage@gmail.com>
2019-07-01 07:55:59 -0400

    kafka stuff

--- kafkaProd.py
+++ kafkaProd.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pickle,os
 import mysql.connector
 
+from kafka import KafkaProducer
+
 train = []
 columns = ['TopBottom','Innings','Outs','Balls','Strikes','ScoreDiff','First','Second','Third','WinningTeam']
 
@@ -66,6 +68,33 @@ def parseInning(inInning):
         inning = '10'
     return (int(topBottom),int(inning))
     
+def connect_kafka_producer():
+    _producer = None
+    try:
+        _producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    except Exception as ex:
+        print('Exception while connecting Kafka')
+        print(str(ex))
+    finally:
+        return _producer
+
+def publish_message(producer_instance, topic_name, key, value):
+    try:
+        key_bytes = bytes(key, encoding='utf-8')
+        value_bytes = bytes(value, encoding='utf-8')
+        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+        producer_instance.flush()
+    except Exception as ex:
+        print('Exception in publishing message')
+        print(str(ex))
+
+def sendToKafka(kafkaProd):
+    pitches = getPitches()
+    for p in pitches:
+        publish_message(kafkaProd,'test','raw',str(p))
 if __name__ == '__main__':
-    getCursor()
+    kafkaProducer = connect_kafka_producer()
+    # publish_message(kafkaProducer,'test','rafddfw','test')
+    sendToKafka(kafkaProducer)
+    # getCursor()
     # getPitches()
